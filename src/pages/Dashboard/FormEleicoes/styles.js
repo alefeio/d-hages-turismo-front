5348a0b30d4c4e6ca5fde089e681bf0f581a36e6
alefeio@gmail.com <alefeio@gmail.com>
2024-06-21 01:36:38 -0300

    style eleitor

--- src/pages/Dashboard/FormEleicoes/styles.js
+++ src/pages/Dashboard/FormEleicoes/styles.js
@@ -68,7 +68,6 @@ export const Container = styled.div`
           align-items: center;
           justify-content: center;
           text-align: left;
-          gap: .5rem;
 
           input, select {
             max-width: 100%;
